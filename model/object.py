15af6bd4fbb8f04e14e6b2a05bdb6cfb19420f45
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2016-05-06 19:24:48 -0300

    Corrigdo problema nesta classe passando os parametros self nos métodos, foi adidiconado um método setPos e getPos

--- model/object.py
+++ model/object.py
@@ -5,6 +5,13 @@ class Object(pygame.Rect):
 	def __init__(self, pos, size):
 		pygame.Rect.__init__(self, pos, size)
 
-	def move(pos):
+	def move(self, pos):
 		self.x = pos[0]
 		self.y = pos[1]
+
+	def setPos(self, pos):
+		self.x = pos[0]
+		self.y = pos[1]
+
+	def getPos(self):
+		return (self.x, self.y)
